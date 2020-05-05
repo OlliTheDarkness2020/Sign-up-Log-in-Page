69e086536f8ea53ea5e02a6b0f96215972935711
Abhiraj Kale <58825394+abhiraj-kale@users.noreply.github.com>
2020-05-05 13:58:15 +0530

    Update
    User getting signed-up and can log in.

--- login.js
+++ login.js
@@ -1,20 +1,17 @@
-var mysql = require('mysql');
 var express = require('express');
 var session = require('express-session');
 var bodyParser = require('body-parser');
 var path = require('path');
-
-var user = {
-    name : "abhiraj",
-    password : "kale"
-}
-
+var mysql      = require('mysql');
 var connection = mysql.createConnection({
-	host     : 'localhost',
-	user     : 'root',
-	password : '',
-	database : 'nodelogin'
+  host     : 'localhost',
+  user     : 'root',
+  password : 'abhiraj',
+  database : 'nodelogin'
 });
+
+connection.connect();
+
 
 var app = express();
 app.use(express.static(path.join(__dirname, "public")));
@@ -33,23 +30,50 @@ app.get('/', function(request, response) {
     
 });
 
+app.post('/signup', (request,response) =>{
+    var username = request.body.username;
+    var password = request.body.password;
+    var email = request.body.email;
+    var details = [username,password,email];
+    
+    var query = connection.query(`INSERT INTO accounts(username,password,email) VALUES (?,?,?)`,[username,password,email],
+     function(error, results) {
+            if(error){
+                throw error;
+            }
+            request.session.loggedin = true; 
+            response.redirect('/');
+            console.log('User created- username: '+ username +' ,password: '+password+' ,email: '+email);
+
+        response.end();
+    });
+});
+
 app.get('/login', (request,response) => {
     response.sendFile(path.join(__dirname + '/login.html'));
 });
 
-app.post('/auth', (request,response) =>{
-    if(request.body.username == user.name && request.body.password== user.password){
-      
-        response.send('User logged in.');
-        
-    }else{
-      
-        response.alert('Please enter Username and Password!');
-		
-    }
-    console.log(request.session.loggedin);
-    response.end();
-   // response.sendFile(path.join(__dirname + '/login.html'));
+app.post('/auth', function(request, response) {
+	var username = request.body.username;
+	var password = request.body.password;
+	if (username && password) {
+		connection.query('SELECT * FROM accounts WHERE username = ? AND password = ?', [username, password], function(error, results, fields) {
+			if (results.length > 0){ 
+                if(results[0].username==username && results[0].password==password){
+                    response.send('Logged in');
+                    request.session.loggedin = true;
+			    	request.session.username = username;  
+                  } 
+			} else {
+                response.send('Incorrect Username and/or Password!');
+                
+			}			
+			response.end();
+		});
+	} else {
+		response.send('Please enter Username and Password!');
+		response.end();
+	}
 });
 
 app.listen(3000, (err) => {
